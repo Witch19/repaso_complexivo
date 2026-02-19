a06c5fda505674a76a57591a6cf55106a0f186cc
JOSS COMPU <josytst.js@gmail.com>
2026-02-19 10:50:59 -0500

    completo

--- cinema_backend/catalog/admin.py
+++ cinema_backend/catalog/admin.py
@@ -4,7 +4,7 @@ from .models import Show, Reservation
 
 @admin.register(Show)
 class ShowAdmin(admin.ModelAdmin):
-    list_display = ("id", "title", "start_time", "room", "price")
+    list_display = ("id", "movie_title", "room", "price", "available_seats")
 
 
 @admin.register(Reservation)
