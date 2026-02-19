ba3c2bd796bee7031f98b169afb74a65de2e4944
JOSS COMPU <josytst.js@gmail.com>
2026-02-19 10:51:51 -0500

    movil

--- cinema_movil/src/api/http.ts
+++ cinema_movil/src/api/http.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const http = axios.create({
-  baseURL: "http://10.0.2.2:8000/api",
+  baseURL: "http://10.0.2.2:8000/api", // Cambia esta URL por la de tu backend
+  //ipv4 localhost para emulador android
 });
